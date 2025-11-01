39021f7c317a06379c816db92fc1ebd0d8db5c2a
Rara Syifa Qaisa <rarasyifaqaisa@email.com>
2025-11-01 20:30:35 +0700

    variabel dan tipe data

---
+++ variabel.py
@@ -0,0 +1,34 @@
+nama = "Rara"
+umur = 22
+tinggi = 143.0
+
+nama_depan = "Rara"
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+
+puisi="""
+Ini adalah puisi
+yang lebih dari
+satu baris
+"""
+
+print(type(puisi))
+
+is_student = True
+is_married = False
+
+print(type(is_student))
+
+skor = 80
+print(skor)
+
+skor = 90
+print(skor)
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+
+print("Luas : ", luas)
