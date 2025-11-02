231be4b4e2a2d71eaad72b6511ff30d8f1ca4600
Rara Syifa Qaisa <rarasyifaqaisa@email.com>
2025-11-02 10:22:29 +0700

    string dan manipulasi string

---
+++ manipulasi_string.py
@@ -0,0 +1,80 @@
+nama = "Rara"
+umur = 22
+
+pesan = "Nama saya " + nama + ", umur " + str(umur)
+print(pesan)
+
+print(len(nama))
+print(len(pesan))
+
+nama = "Python"
+print(nama[0])
+print(nama[1])
+print(nama[2])
+
+print(nama[-1])
+print(nama[-2])
+print(nama[-3])
+
+nama = "Python"
+print(nama[:])
+print(nama[0:3])
+print(nama[2:])
+
+nama = "Alice"
+print(nama)
+nama_upper = nama.upper()
+print(nama_upper)
+nama_lower = nama.lower()
+print(nama_lower)
+
+nama = "rara syifa"
+nama_title = nama.title()
+print(nama_title)
+nama_capilized = nama.capitalize()
+print(nama_capilized)
+
+nama = "   Rara    "
+name_strip = nama.strip()
+print(name_strip)
+
+kalimat = "I love Java"
+kalimat_baru = kalimat.replace("Java", "Python")
+print(kalimat_baru)
+
+nama = "Rara Syifa Qaisa"
+jumlah_k = nama.count("Rara")
+print(jumlah_k)
+
+kalimat = "Python Programming"
+posisi = kalimat.find("Programming")
+print(posisi)
+
+kalimat = "Baris pertama\nBaris kedua"
+print(kalimat)
+
+kalimat = "Nama:\tRara\nUmur:\t22"
+print(kalimat)
+
+lokasi = "C:\\\\Users\\\\Rara\\\Desktop"
+print(lokasi)
+
+kalimat = "Dia berkata \"Hello\" kepada saya"
+print(kalimat)
+
+nama = "Rara"
+umur = 22
+kota = "Jakarta"
+
+kalimat = f"Halo, nama saya {nama}, umur saya {umur}, tinggal di {kota}"
+print(kalimat)
+
+harga = 10000
+jumlah = 3
+
+total = f"Total : {harga * jumlah}"
+print(total)
+
+nama = "Rara Syifa"
+kalimat = f"Hello {nama.upper()}"
+print(kalimat)
