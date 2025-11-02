2e71c8e661617f9f2e634880f2654d6e0da8f3e9
Rara Syifa Qaisa <rarasyifaqaisa@email.com>
2025-11-02 10:36:33 +0700

    operator

---
+++ operator.py
@@ -0,0 +1,64 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print(a // b)
+print(a % b)
+
+print(10 % 2)
+print(11 % 2)
+
+a = 2
+b = 3
+
+print(a ** b)
+
+x = 10
+print(x)
+
+x += 5
+print(x)
+
+x -= 3
+print(x)
+
+a = 2
+b = 3
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+nama1 = "Eko"
+nama2 = "Joko"
+nama3 = "Eko"
+
+print(nama1 == nama2)
+print(nama1 == nama3)
+
+umur = 25
+print(umur > 18 and umur < 30)
+
+hari = "Sabtu"
+print(hari == "Sabtu" or hari == "Minggu")
+
+aktif = True
+print(not aktif)
+
+nama_depan = "Eko"
+nama_belakang = "Khannedy"
+nama_lengkap = nama_depan + " " + nama_belakang
+print(nama_lengkap)
+
+garis = "-"
+print(garis * 30)
+
+kalimat = "Python adalah bahasa pemrograman"
+print("Python" in kalimat)
+print("Java" in kalimat)
+print("bahasa" in kalimat)
